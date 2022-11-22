cd9de2323564599c38b4a77be7a401d0cfa5a1dc
blueskymanner <page111first@gmail.com>
2022-11-23 09:44:08 +1000

    created assets

---
+++ Metaverse-Rights-Version-2/assets/index.js
@@ -0,0 +1,8 @@
+import logo from './logo.png'
+import walletwhite from './wallet.png'
+import banner from './banner.png'
+import bannergradient from './bannergradient.png'
+import tm from './tm.png'
+import eye from './eye.png'
+
+export default { eye, tm, logo, walletwhite, banner, bannergradient }
